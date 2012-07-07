5afb06685fea5fb3207601fd72a4e17fd7f75e07
Chase Southard <chase.southard@gmail.com>
2012-07-06 23:31:11 -0400

    adding admin.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  attr_accessor :login
+  attr_accessible :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login).downcase
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  end
 end
