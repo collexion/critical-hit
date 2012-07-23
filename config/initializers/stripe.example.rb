99e07d01ae5bfdcbed72fa40f30ab82793772758
Chase Southard <chase.southard@gmail.com>
2012-07-22 20:04:34 -0400

    adds comments

--- config/initializers/stripe.example.rb
+++ config/initializers/stripe.example.rb
@@ -1,2 +1,5 @@
+# Create a new file called stripe.rb and copy and uncomment the last two lines.
+# Then add your own private and publishable keys.
+
 #Stripe.api_key = "my_private_key"
 #STRIPE_PUBLIC_KEY = "pk_my_public_key"
